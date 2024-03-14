4f98e515c34ad5786b94b7bc961af8cb3e9bcfd7
Maria <mariafcknt@outlook.com>
2024-03-14 14:37:41 -0300

    update to the latest class

--- revisao/src/App.java
+++ revisao/src/App.java
@@ -1,22 +1,48 @@
 import entidades.Cidade;
 import entidades.Cliente;
+import entidades.Pokemon;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
 
-        Cliente umCliente;
-        umCliente = new Cliente("Ana");
+        //Cliente umCliente;
+        //umCliente = new Cliente("Ana");
 
         //System.out.println(umCliente);
         //System.out.println(umCliente.getNome());
         
 
+        //var umaCidade = new Cidade("Joinville");
+        //umCliente.setCidade(umaCidade);
+        //System.out.println(umCliente.getCidade().getNome());
+
+        //System.out.println(umCliente.toString());
+
+        float peso = 100;
+        int idade = 20;
+        var nome = "zezinho";
+        //variavel
+        Cliente umCliente;
+        //instanciação de um objeto
+        umCliente = new Cliente("Zezinho");
+        System.out.println(umCliente);
+        System.out.println(umCliente.getNome());
+
         var umaCidade = new Cidade("Joinville");
         umCliente.setCidade(umaCidade);
         System.out.println(umCliente.getCidade().getNome());
 
-        //System.out.println(umCliente.toString());
+        var bulbasaur = new Pokemon("Bulbasaur");
+        var jigglypuff = new Pokemon("Jigglypuff");
+
+        umCliente.getListaPokemon().add(bulbasaur);
+        umCliente.getListaPokemon().add(jigglypuff);
+        // umCliente.getListaPokemon().add("PEDRA");
+        // umCliente.getListaPokemon().add(2134234234);
+        // umCliente.getListaPokemon().add(false);
+        
+        System.out.println(umCliente.getListaPokemon());
 
     }
 }
--- revisao/src/entidades/Cliente.java
+++ revisao/src/entidades/Cliente.java
@@ -1,36 +1,29 @@
 package entidades;
 
+import java.util.ArrayList;
 public class Cliente {
     private String nome;
     private String endereco;
     private Cidade cidade;
+    private ArrayList<Pokemon> listaPokemon 
+            = new ArrayList<Pokemon>();
 
-    public Cliente(String nome){
-        setNome(nome);
+
+    public ArrayList<Pokemon> getListaPokemon() {
+        return listaPokemon;
     }
 
-    //override - sobreescrita de método
-    //para haver uma sobreescrita é necessária uma herança
-    public String toString(){ //o método toString() retorna a representação de um objeto
+    public void setListaPokemon(ArrayList<Pokemon> listaPokemon) {
+        this.listaPokemon = listaPokemon;
+    }
+
+
+
+    //override - sobreescrita de metodo
+    public String toString(){
         return getNome();
     }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
     
-    public String getNome(){
-        return nome;
-    }
-
-    public void setEndereco(String endereco){
-        this.endereco = endereco;
-    }
-
-    public String getEndereco(){
-        return endereco;
-    }
-
     public Cidade getCidade() {
         return cidade;
     }
@@ -38,5 +31,27 @@ public class Cliente {
     public void setCidade(Cidade cidade) {
         this.cidade = cidade;
     }
+
+    public Cliente(String nome){
+        setNome(nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    
     
 }

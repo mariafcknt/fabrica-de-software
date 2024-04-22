eee06eba14228d372484f4dae2b61d0bd5054885
Maria <mariafcknt@outlook.com>
2024-04-22 20:30:45 -0300

    aula 22/04 - adicionar no banco de dados

--- projeto-spring/projfso2024b/src/main/java/br/univille/projfso2024b/controller/ClienteController.java
+++ projeto-spring/projfso2024b/src/main/java/br/univille/projfso2024b/controller/ClienteController.java
@@ -3,9 +3,11 @@ package br.univille.projfso2024b.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.projfso2024b.entity.Cliente;
 import br.univille.projfso2024b.service.ClienteService;
 
 @Controller
@@ -20,5 +22,17 @@ public class ClienteController {
         var listaClientes = service.getAll();
         //html, referência/apelido do dado no html, dado
         return new ModelAndView("cliente/index", "listaClientes", listaClientes);
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novo(){
+        var cliente = new Cliente();
+        return new ModelAndView("cliente/form","cliente", cliente);
+    }
+
+    @PostMapping()
+    public ModelAndView save(Cliente cliente){
+        service.save(cliente);
+        return new ModelAndView("redirect:/clientes");
     }
 }
--- projeto-spring/projfso2024b/src/main/java/br/univille/projfso2024b/entity/Cliente.java
+++ projeto-spring/projfso2024b/src/main/java/br/univille/projfso2024b/entity/Cliente.java
@@ -1,6 +1,9 @@
 package br.univille.projfso2024b.entity;
 
 import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,6 +24,7 @@ public class Cliente {
     @Column(length = 1000)
     private String endereco;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataNascimento;
 
     public long getId() {

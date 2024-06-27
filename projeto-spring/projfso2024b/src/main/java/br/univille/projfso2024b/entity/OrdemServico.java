552ad59aaecdf7db7fd580f8d2140ecfb3185e24
Yduan Rech Effting <105087789+yduanrech@users.noreply.github.com>
2024-06-27 19:42:36 -0300

    add class OrdemServico

--- projeto-spring/projfso2024b/src/main/java/br/univille/projfso2024b/entity/OrdemServico.java
+++ projeto-spring/projfso2024b/src/main/java/br/univille/projfso2024b/entity/OrdemServico.java
@@ -13,62 +13,39 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
-public class Pet {
+public class OrdemServico {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id; //chave artificial
+
     @Column(length = 1000, nullable = false)
-    @NotBlank(message = "Campo não pode ser branco")    
-    private String nome;
+    @NotBlank(message = "Campo não pode ser branco")
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dataEntrada;
+
     @Column(length = 1000)
-    private int idade;
+    private String servico;
+    
     @Column(length = 1000)
-    private String especie;
+    private String anotacao;
+    
+    @Column(length = 1000)
+    private String formaPagamento;
+    
     @Column(length = 1000)
     private String raca;
 
     @ManyToOne(cascade = CascadeType.ALL) //ALL = Se apaga o cliente, apaga o Pet. Para isso não acontecer fazemos cascade = {CascadeType.MERGE, CascadeType.REFRESH} refresh(quando busca o cliente, busca a cidade, para trazer o dado mais atual)
-    private Cliente cliente;
+    private Pet Pet;
 
-    public Cliente getCliente() {
-        return cliente;
-    }
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public String getEspecie() {
-        return especie;
-    }
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-    public String getRaca() {
-        return raca;
-    }
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
+    
     /* 
     public Cliente getCliente() {
         return cliente;

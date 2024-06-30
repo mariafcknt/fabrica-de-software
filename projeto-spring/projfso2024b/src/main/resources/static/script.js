0c367aea5fee5e7c56dc5ae28728f55310d654d0
Yduan Rech Effting <105087789+yduanrech@users.noreply.github.com>
2024-06-30 19:56:15 -0300

    fix ordens e inicio

--- projeto-spring/projfso2024b/src/main/resources/static/script.js
+++ projeto-spring/projfso2024b/src/main/resources/static/script.js
@@ -40,4 +40,24 @@
         })
     })
 
+    $("#tabordens").on("click", ".js-delete", function(){
+        //alert("ema ema ema");
+        let botaoClicado = $(this); //Pegar a referencia de qua lbotao vermelho foi clicado
+        $("#btnsim").attr("data-id", botaoClicado.attr("data-id")); //Pegar o id do botao clicado e passar para o botao sim
+        $("#modalpet").modal("show"); //aparecer a modal na cara do usuário
+    })
+
+    //aqui vamos chamar o botao sim para deletar o cliente
+    $("#btnsim").on("click", function(){
+        let botaoSim = $(this);
+        let id = botaoSim.attr("data-id");
+        $.ajax({
+            url: "/ordens/delete/" + id,//para onde vamos chamar, qual endpoint
+             method: "GET", //qual metodo vamos usar
+            success: function(){
+            window.location.href = "/ordens";
+        }
+        })
+    })
+
 })();
